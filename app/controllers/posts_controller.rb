44c0f0fccef46b9c3ddfa04b0d8c7fd4e6cfe1a9
Florian HAMEL <f.hamel64@gmail.com>
2019-02-01 18:17:35 +0100

    problme avec jquery

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :status, :category_ids, :content)
+  end
+end
